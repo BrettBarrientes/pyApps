c5b679bb27ec72e9572172624c0d3c198f4a993b
BrettBarrientes <b.barrientes25@outlook.com>
2023-09-07 08:21:39 -0400

    Updated and added new images to go along with the description

--- weatherApp.py
+++ weatherApp.py
@@ -13,15 +13,7 @@ root.geometry('400x400')
 root['background'] = 'lightblue'
 
 
-# image
-img = ImageTk.PhotoImage(Image.open('weather.png'))
-panel = Label(root, image = img)
-panel.place(x=0, y=520)
-
-API_key = '0919b78bbb94ccf36776d94b0d0c94e6'
-
-#city = input("Enter a city: ")
-#zip_code = input("Enter a zip code: ")
+API_key = 'OpendWeatherMapAPIKey'
 
 city_name = StringVar()
 city_entry = Entry(root, textvariable=city_name)
@@ -32,16 +24,16 @@ zip_code = StringVar()
 zip_code_entry = Entry(root, textvariable=zip_code)
 zip_code_entry.place(x=160, y=30)
 
-#pprint(weather_data)
 
 if zip_code_entry.get() == '' and city_entry.get() == '':
     
     def zip_code():
         api_request= requests.get("http://api.openweathermap.org/data/2.5/weather?appid="+API_key+"&zip="+zip_code_entry.get())
         api = json.loads(api_request.content)
+        global new_image
         try:
             weather_data = api
-            
+                
             city_name = weather_data['name']
 
             temp = weather_data['main']['temp']
@@ -52,16 +44,56 @@ if zip_code_entry.get() == '' and city_entry.get() == '':
 
             dt = datetime.datetime.fromtimestamp(weather_data['dt'])
 
+            date = dt.strftime('%b-%d-%Y')
+
             label_city.configure(text=city_name)
             label_fahrenheit.configure(text=fahrenheit)
             label_description.configure(text=description)
-            label_dt.configure(text=dt)        
+            label_dt.configure(text=date)      
 
             #print('\nCity Name : ', city_name)
             #print('\nTemperature : ', fahrenheit)
             #print('\nDescription : ', description)
         except:
+            
             label_reminder.configure(text='Please enter a valid city name and zip code')
+
+        if description == 'clear sky':
+            img = Image.open('clear_sky.png')
+            image = img.resize((100, 100))
+            new_image = ImageTk.PhotoImage(image)
+            panel = Label(root, image = new_image)
+            panel.place(x=16, y=110)
+        elif description == 'thunderstorm':
+            img = Image.open('thunderstorm.png')
+            image = img.resize((100, 100))
+            new_image = ImageTk.PhotoImage(image)
+            panel = Label(root, image = new_image)
+            panel.place(x=16, y=110)
+        elif description == 'scattered clouds':
+            img = Image.open('scattered_clouds.png')
+            image = img.resize((100, 100))
+            new_image = ImageTk.PhotoImage(image)
+            panel = Label(root, image = new_image)
+            panel.place(x=16, y=110)
+        elif description == 'windy':
+            img = Image.open('windy.png')
+            image = img.resize((100, 100))
+            new_image = ImageTk.PhotoImage(image)
+            panel = Label(root, image = new_image)
+            panel.place(x=16, y=110)
+        elif description == 'Rainy':
+            img = Image.open('Rainy.png')
+            image = img.resize((100, 100))
+            new_image = ImageTk.PhotoImage(image)
+            panel = Label(root, image = new_image)
+            panel.place(x=16, y=110)
+        elif description == 'snow':
+            img = Image.open('snow.png')
+            image = img.resize((100, 100))
+            new_image = ImageTk.PhotoImage(image)
+            panel = Label(root, image = new_image)
+            panel.place(x=16, y=110)
 else:   
     label_reminder = Label(root, text="Please enter a city name and zip code", width=0, bg='lightblue', font=("bold", 10))
     label_reminder.place(x=7, y=3)
@@ -86,10 +118,6 @@ label_description.place(x=16, y=293)
 
 label_dt = Label(root, text="...", width=0, bg='lightblue',  font=("Helvetica", 15), fg='black')
 label_dt.place(x=16, y=343)
-
-img = ImageTk.PhotoImage(Image.open('weather.png'))
-panel = Label(root, image=img, width=125, height=110)
-panel.place(x=16, y=110)
 
 root.mainloop()
 
