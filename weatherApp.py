8a3163872c36b2177b8b3173e0788d65e1b0c863
BrettBarrientes <b.barrientes25@outlook.com>
2023-09-11 20:50:08 -0400

    shorten the if/else to iterate through the weather description, added wind speed and Min/Max temp

--- weatherApp.py
+++ weatherApp.py
@@ -13,7 +13,7 @@ root.geometry('400x400')
 root['background'] = 'lightblue'
 
 
-API_key = 'OpendWeatherMapAPIKey'
+API_key = 'Enter API KEY Here'
 
 city_name = StringVar()
 city_entry = Entry(root, textvariable=city_name)
@@ -38,18 +38,33 @@ if zip_code_entry.get() == '' and city_entry.get() == '':
 
             temp = weather_data['main']['temp']
 
+            temp_min = weather_data['main']['temp_min']
+
+            temp_max = weather_data['main']['temp_max']
+
             description = weather_data['weather'][0]['description']
 
-            fahrenheit = round((temp - 273.15) * 9/5 + 32)
+            fahrenheit_temp = round((temp - 273.15) * 9/5 + 32)
+
+            fahrenheit_temp_min = round((temp_min - 273.15) * 9/5 + 32)
+
+            fahrenheit_temp_max = round((temp_max - 273.15) * 9/5 + 32)
 
             dt = datetime.datetime.fromtimestamp(weather_data['dt'])
 
             date = dt.strftime('%b-%d-%Y')
 
+            convert_meters_per_second_to_miles_per_hour = round(weather_data['wind']['speed'] * 2.23694, 2)
+
             label_city.configure(text=city_name)
-            label_fahrenheit.configure(text=fahrenheit)
+            label_fahrenheit_temp.configure(text=fahrenheit_temp)
+            label_fahrenheit_temp_min.configure(text=fahrenheit_temp_min)
+            label_fahrenheit_temp_max.configure(text=fahrenheit_temp_min)
             label_description.configure(text=description)
-            label_dt.configure(text=date)      
+            label_dt.configure(text=date)
+            label_mph.configure(text=convert_meters_per_second_to_miles_per_hour)  
+
+            #pprint(weather_data)    
 
             #print('\nCity Name : ', city_name)
             #print('\nTemperature : ', fahrenheit)
@@ -60,40 +75,23 @@ if zip_code_entry.get() == '' and city_entry.get() == '':
 
         if description == 'clear sky':
             img = Image.open('clear_sky.png')
-            image = img.resize((100, 100))
-            new_image = ImageTk.PhotoImage(image)
-            panel = Label(root, image = new_image)
-            panel.place(x=16, y=110)
         elif description == 'thunderstorm':
             img = Image.open('thunderstorm.png')
-            image = img.resize((100, 100))
-            new_image = ImageTk.PhotoImage(image)
-            panel = Label(root, image = new_image)
-            panel.place(x=16, y=110)
         elif description == 'scattered clouds':
             img = Image.open('scattered_clouds.png')
-            image = img.resize((100, 100))
-            new_image = ImageTk.PhotoImage(image)
-            panel = Label(root, image = new_image)
-            panel.place(x=16, y=110)
         elif description == 'windy':
             img = Image.open('windy.png')
-            image = img.resize((100, 100))
-            new_image = ImageTk.PhotoImage(image)
-            panel = Label(root, image = new_image)
-            panel.place(x=16, y=110)
         elif description == 'Rainy':
             img = Image.open('Rainy.png')
-            image = img.resize((100, 100))
-            new_image = ImageTk.PhotoImage(image)
-            panel = Label(root, image = new_image)
-            panel.place(x=16, y=110)
         elif description == 'snow':
             img = Image.open('snow.png')
-            image = img.resize((100, 100))
-            new_image = ImageTk.PhotoImage(image)
-            panel = Label(root, image = new_image)
-            panel.place(x=16, y=110)
+        elif description == 'broken clouds':
+            img = Image.open('broken_clouds.png')
+
+        image = img.resize((100, 100))
+        new_image = ImageTk.PhotoImage(image)
+        panel = Label(root, image = new_image)
+        panel.place(x=16, y=110)
 else:   
     label_reminder = Label(root, text="Please enter a city name and zip code", width=0, bg='lightblue', font=("bold", 10))
     label_reminder.place(x=7, y=3)
@@ -110,16 +108,37 @@ zip_codeButton.place(x=160, y=50)
 label_city = Label(root, text="...", width=0, bg='lightblue', font=("bold", 15))
 label_city.place(x=16, y=78)
 
-label_fahrenheit = Label(root, text="...", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
-label_fahrenheit.place(x=16, y=238)
+label_fahrenheit_temp_current_name = Label(root, text="Current Temp:", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
+label_fahrenheit_temp_current_name.place(x=125, y=120)
+
+label_fahrenheit_temp = Label(root, text="...", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
+label_fahrenheit_temp.place(x=255, y=120)
+
+label_fahrenheit_temp_min_name = Label(root, text="Temp Min:", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
+label_fahrenheit_temp_min_name.place(x=16, y=238)
+
+label_fahrenheit_temp_min = Label(root, text="...", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
+label_fahrenheit_temp_min.place(x=113, y=238)
+
+label_fahrenheit_temp_max_name = Label(root, text="Temp Max:", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
+label_fahrenheit_temp_max_name.place(x=16, y=268)
+
+label_fahrenheit_temp_max = Label(root, text="...", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
+label_fahrenheit_temp_max.place(x=118, y=268)
 
 label_description = Label(root, text="...", width=0, bg='lightblue', font=("Helvetica", 15), fg='black' )
-label_description.place(x=16, y=293)
+label_description.place(x=16, y=303)
 
 label_dt = Label(root, text="...", width=0, bg='lightblue',  font=("Helvetica", 15), fg='black')
 label_dt.place(x=16, y=343)
 
+label_mph_name = Label(root, text="Wind Speed (mph):", width=0, bg='lightblue', font=("Helvetica", 15), fg='black')
+label_mph_name.place(x=125, y=150)
+
+label_mph = Label(root, text="...", width=0, bg='lightblue',  font=("Helvetica", 15), fg='black')
+label_mph.place(x=300, y=150)
+
 root.mainloop()
 
 
-
+# Will add Haze description and image in next update
